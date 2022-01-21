8bcac8f26cd73045b93d5a7825082706a10284e4
paulBaudouin <paulbaudouin@zohomail.eu>
2022-01-21 11:54:47 +0100

    methode crud component role et facture

--- src/app/pages/role/role.component.ts
+++ src/app/pages/role/role.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Role } from 'src/app/model/role';
+import { RoleService } from 'src/app/service/role.service';
 
 @Component({
   selector: 'app-role',
   templateUrl: './role.component.html',
-  styleUrls: ['./role.component.scss']
+  styleUrls: ['./role.component.css']
 })
 export class RoleComponent implements OnInit {
-
-  constructor() { }
+  roles: any;
+  role: Role=new Role();
+  constructor(private roleService:RoleService) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
+  findAll(){
+    this.roleService.findAll().subscribe(data =>{this.roles = data});
+  // subscribe : rafraichit la table
+  }
+  deleteRole(id:number){
+    this.roleService.deleteRole(id).subscribe( () =>{this.findAll()} );
+  }
+
+  saveRole(){
+    this.roleService.saveRole(this.role).subscribe( () => {
+      this.findAll(); // mettre à jour la table
+      this.role = new Role(); // pour vider le formulaire
+    })
+  }
 }
