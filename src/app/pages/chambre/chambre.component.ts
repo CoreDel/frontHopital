199859d220696cd46217dca3e5984a5d0232124c
VeroGui <veroguillaume@hotmail.fr>
2022-01-21 11:47:48 +0100

    testfronfron

--- src/app/pages/chambre/chambre.component.ts
+++ src/app/pages/chambre/chambre.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chambre } from 'src/app/model/chambre';
+import { ChambreService } from 'src/app/service/chambre.service';
 
 @Component({
   selector: 'app-chambre',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChambreComponent implements OnInit {
 
-  constructor() { }
+  chambres: any;
+  chambre: Chambre= new Chambre;
+
+  constructor(private chambreService: ChambreService) { }
 
   ngOnInit(): void {
   }
-
+  findAll(){
+    this.chambreService.findAll().subscribe(data => {this.chambre =data})
+  }
+  deleteChambre(id: number) {
+    this.chambreService.delete(id).subscribe(() => { this.findAll()})
+  }
+  saveChambre() {
+    this.chambreService.save(this.chambre).subscribe(() => {
+      this.findAll(); // Mettre à jour la table
+      this.chambre = new Chambre(); // pour vider le formulaire
+    })
+  }
 }
