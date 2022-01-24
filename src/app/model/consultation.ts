e2a725d09c80ce68c9a780a841442f44c87ef024
nobelios <nobelios@gmail.com>
2022-01-24 18:19:59 +0100

    first commit

--- src/app/model/consultation.ts
+++ src/app/model/consultation.ts
@@ -1,2 +1,10 @@
+import { Utilisateur } from "./utilisateur";
+import { Ordonnance } from "./ordonnance";
+
 export class Consultation {
+    idConsultation!:number;
+    idPatient!:number;
+    idMedecin!:number;
+    motifConsultation!:string;
+    date!:number;
 }
