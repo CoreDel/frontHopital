c140fcb04d85446c4761d72e2ab924d910d10fb4
VeroGui <veroguillaume@hotmail.fr>
2022-01-26 14:38:44 +0100

    des petits trous

--- src/app/pages/hopital/hopital.component.ts
+++ src/app/pages/hopital/hopital.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hopital } from 'src/app/model/hopital';
+import { Medicament } from 'src/app/model/medicament';
+import { Utilisateur } from 'src/app/model/utilisateur';
 import { HopitalService } from 'src/app/service/hopital.service';
 
 @Component({
@@ -11,12 +13,20 @@ export class HopitalComponent implements OnInit {
   hopitals: any;
   hopital: Hopital = new Hopital;
   nbChambre: number;
+  nbMedecin: number;
+  nbPatient: number;
+  medecins: Utilisateur[];
+  medicaments: Medicament[];
 
   constructor(private hopitalService: HopitalService) { }
 
   ngOnInit(): void {
     this.findAll()
     this.nombreDispoChambre();
+    this.nombreMedecin();
+    this.nombrePatient();
+    this.listMedecin();
+    this.listMedicament();
   }
   findAll(){
     this.hopitalService.findAll().subscribe(data => {this.hopital =data})
@@ -31,8 +41,18 @@ export class HopitalComponent implements OnInit {
     })
   }
   nombreDispoChambre() {
-   
     this.hopitalService.nombreDispoChambre().subscribe( data => {this.nbChambre= data});
-   
+  }
+  nombreMedecin(){
+    this.hopitalService.nombreMedecin().subscribe(data =>{this.nbMedecin= data});
+  }
+  nombrePatient(){
+    this.hopitalService.nombrePatient().subscribe(data =>{this.nbPatient= data});
+  }
+  listMedecin(){
+    this.hopitalService.listMedecin().subscribe(data =>{this.medecins= data});
+  }
+  listMedicament(){
+    this.hopitalService.listMedicament().subscribe(data =>{this.medicaments= data});
   }
 }
